dd51ec0b78da5d437a8d61ebaf14001a683b4d22
thinhdz1500 <thinhdz1500@gmail.com>
2024-11-06 14:19:54 +0700

    init project Week5

---
+++ LabWeek5/src/main/java/com/thinne/LabWeek5Application.java
@@ -0,0 +1,13 @@
+package com.thinne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabWeek5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabWeek5Application.class, args);
+    }
+
+}
